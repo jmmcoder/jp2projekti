991db55fe2109e20c037422f91a93a070fe0e03f
jmmcoder <a1400154@myy.haaga-helia.fi>
2016-05-16 11:21:15 +0000

    MySQL ja Python

---
+++ sensorikantaan.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# Python-ohjelma, joka lisää dataa MySQL-tietokantaan
+# Lisättävä tieto haetaan sensoriohjelmalta 
+
+# Tarvittavat kirjastot ja tiedostot
+import MySQLdb
+import sensori.py
+
+# Avataan tietokantayhteys
+db	= [TIETOKANNAN OSOITE, KÄYTTÄJÄ JA SALASANA]
+
+# Cursor-olio, joka vaaditaan tietokantaohjelmoinnissa
+cursor = db.cursor()
+
+# Valmistellaan SQL-lauseke
+# Haetaan sensoridata sensoriohjelmalta
+sql	= """[INSERT LAUSEKE TÄHÄN]"""
+
+try:
+	# Suoritetaan SQL-lauseke
+	cursor.execute(sql)
+	# Lisätään data kantaan
+	db.commit()
+except:
+	# Rollback, mikäli ilmenee virhe
+	db.rollback()
+
+# Suljetaan lopuksi tietokantayhetys
+db.close()
